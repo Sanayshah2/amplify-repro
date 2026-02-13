9dd7578afa118b3f478499ddfb2b7a95257e2f3c
Sanay Yogesh Shah <sanayyog@amazon.com>
2026-02-13 15:59:53 -0800

    initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "S3Trigger332680be": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
---
+++ amplify/hooks/pre-delete.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash -e
+#
+# Amplify-deleteコマンドの無効化
+exit 1
