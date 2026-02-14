819c51b93f9c9c37879cf3cfe89c2c0ec68e4bf5
Sanay Yogesh Shah <sanayyog@amazon.com>
2026-02-13 17:33:33 -0800

    commit

--- amplify/backend/function/S3Trigger332680be/src/index.js
+++ amplify/backend/function/S3Trigger332680be/src/index.js
@@ -1,3 +1,10 @@
-exports.handler = async (event) => {
-  return { statusCode: 200, body: 'placeholder' };
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = void 0;
+const uuid_1 = require("uuid");
+const handler = async (event) => {
+    const id = (0, uuid_1.v4)();
+    console.log('Processing event:', id);
+    return { statusCode: 200, body: JSON.stringify({ id }) };
 };
+exports.handler = handler;
